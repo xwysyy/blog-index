d18ffc225b1e75292f6b3c6e9ff1e02ad45a4563
xwysyy <118731667+xwysyy@users.noreply.github.com>
2023-08-09 01:05:17 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,15 +4,15 @@ const HOST = ""; // 线上服务器地址，如果不使用请留空
 
 const BLOG_NAME = "我的博客"; // 个人网站名字
 
-const BLOG_URL = "https://blog.esunr.xyz"; // 个人网站链接（请填写完整链接）
+const BLOG_URL = "https://www.fuyiran.icu"; // 个人网站链接（请填写完整链接）
 
-const GITHUB = "https://github.com/EsunR"; // 右上角 Github 图标链接，留空不显示
+const GITHUB = "https://github.com/xwysyy"; // 右上角 Github 图标链接，留空不显示
 
 const BACKGROUND_IMG_URL = "https://api.dujin.org/bing/1920.php"; // 背景图片url，默认为必应每日图片（第三方接口）
 
 const SORT_DATA = [
-  { sortId: 1, title: "示例分类1" },
-  { sortId: 2, title: "示例分类2" }
+  { sortId: 1, title: "博客线路" },
+  { sortId: 2, title: "部署项目" }
 ]; // 网站分类列表，请按照API.md中'/getSort'接口中返回的data格式填写
 
 const RECORD_NUMBER = ""; // 网站备案号，留空不显示
@@ -37,15 +37,7 @@ const PAGES_DATA = [
 ]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
 
 const SLOGAN = [
-  "欢迎拜访",
-  "歡迎拜訪",
-  "Welcome, my friend!",
-  "訪問へようこそ",
-  "嗨，别来无恙",
-  "不忘初心，一生浪漫",
-  "最近还好吗？",
-  "流星，落花，萤火",
-  "马车越空，晃荡越响"
+  "像我这样善良的人"
 ]; // 在Logo下方显示的标题
 
 const FOOTER_INFO = true; // 是否显示抽屉右下角的文字信息
